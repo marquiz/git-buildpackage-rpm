6e00553cbb736900d8c0fa114ec43fc82e70cac2
Markus Lehtonen <markus.lehtonen@linux.intel.com>
2012-07-09 17:19:27 +0300

    UpstreamSource: introduce is_tarball() method
    New method for checking if the sources are a tarball (possibly for
    checking if the sources are committable by pristine-tar).
    
    Signed-off-by: Markus Lehtonen <markus.lehtonen@linux.intel.com>

--- tests/06_test_upstream_source.py
+++ tests/06_test_upstream_source.py
@@ -24,6 +24,7 @@ class TestDir(unittest.TestCase):
         """Upstream source is a directory"""
         source = UpstreamSource(self.upstream_dir)
         self.assertEqual(source.is_orig(), False)
+        self.assertEqual(source.is_tarball(), False)
         self.assertEqual(source.path, self.upstream_dir)
         self.assertEqual(source.unpacked, self.upstream_dir)
         self.assertEqual(source.guess_version(), ('test', '1.0'))
@@ -59,6 +60,7 @@ class TestTar(unittest.TestCase):
         target = self.tmpdir.join("gbp_0.1.tar.bz2")
         repacked = self.source.pack(target)
         self.assertEqual(repacked.is_orig(), True)
+        self.assertEqual(repacked.is_tarball(), True)
         self.assertEqual(repacked.is_dir(), False)
         self._check_tar(repacked, ["gbp/errors.py", "gbp/__init__.py"])
 
@@ -67,6 +69,7 @@ class TestTar(unittest.TestCase):
         target = self.tmpdir.join("gbp_0.1.tar.bz2")
         repacked = self.source.pack(target, ["__init__.py"])
         self.assertEqual(repacked.is_orig(), True)
+        self.assertEqual(repacked.is_tarball(), True)
         self.assertEqual(repacked.is_dir(), False)
         self._check_tar(repacked, ["gbp/errors.py"],
                                   ["gbp/__init__.py"])
@@ -97,6 +100,7 @@ class TestZip(unittest.TestCase):
     def test_unpack(self):
         source = UpstreamSource(self.zipfile)
         self.assertEqual(source.is_orig(), False)
+        self.assertEqual(source.is_tarball(), False)
         self.assertEqual(source.is_dir(), False)
         self.assertEqual(source.unpacked, None)
         source.unpack(str(self.tmpdir))
