54c8b592f93e0051e5a0f033949cb91638636a05
Markus Lehtonen <markus.lehtonen@linux.intel.com>
2012-11-08 12:27:43 +0200

    buildpackage-rpm: fail patch-export if local patches found
    Make patch export fail if local patch files (not marked for manual
    maintenance) exist.
    
    Signed-off-by: Markus Lehtonen <markus.lehtonen@linux.intel.com>

--- gbp/scripts/buildpackage_rpm.py
+++ gbp/scripts/buildpackage_rpm.py
@@ -227,6 +227,15 @@ def export_patches(repo, spec, export_treeish, options):
     """
     Generate patches and update spec file
     """
+    # Fail if we have local patch files not marked for manual maintenance.
+    # Ignore patches listed in spec but not found in packaging dir
+    for patch in spec.patchseries():
+        if os.path.exists(patch.path):
+            raise GbpAutoGenerateError(
+                    'Patches not marked for manual maintenance found, '
+                    'refusing to overwrite! Fix by applying them to packaging '
+                    'branch and removing the files.')
+
     try:
         upstream_tree = get_upstream_tree(repo, spec, options)
         update_patch_series(repo, spec, upstream_tree, export_treeish, options)
