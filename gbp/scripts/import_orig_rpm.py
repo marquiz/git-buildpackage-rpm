555fcb2b7c2fe8d681bac1b8ef8d53bb66909eda
Markus Lehtonen <markus.lehtonen@linux.intel.com>
2012-11-13 11:50:34 +0200

    rpm: configuration option for setting the output color scheme
    Adds the new 'color-scheme' option to the rpm tools.
    
    Signed-off-by: Markus Lehtonen <markus.lehtonen@linux.intel.com>

--- gbp/scripts/import_orig_rpm.py
+++ gbp/scripts/import_orig_rpm.py
@@ -185,10 +185,12 @@ def parse_args(argv):
     parser.add_option("-v", "--verbose", action="store_true", dest="verbose", default=False,
                       help="verbose command execution")
     parser.add_config_file_option(option_name="color", dest="color", type='tristate')
+    parser.add_config_file_option(option_name="color-scheme",
+                                  dest="color_scheme")
     parser.add_config_file_option(option_name="tmp-dir", dest="tmp_dir")
 
     (options, args) = parser.parse_args(argv[1:])
-    gbp.log.setup(options.color, options.verbose)
+    gbp.log.setup(options.color, options.verbose, options.color_scheme)
 
     return options, args
 
