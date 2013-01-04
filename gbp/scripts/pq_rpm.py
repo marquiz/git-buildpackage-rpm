aecfc649d6825acc147f3009577bfeb688bdc874
Markus Lehtonen <markus.lehtonen@linux.intel.com>
2013-01-04 15:10:02 +0200

    rpm.SpecFile: change specfile attribute to represent filename
    There is specdir to get the (absolute) directory path.
    
    Signed-off-by: Markus Lehtonen <markus.lehtonen@linux.intel.com>

--- gbp/scripts/pq_rpm.py
+++ gbp/scripts/pq_rpm.py
@@ -337,7 +337,7 @@ def import_spec_patches(repo, branch, options):
 
     repo.set_branch(branch)
 
-    return os.path.basename(spec.specfile)
+    return spec.specfile
 
 
 def rebase_pq(repo, branch, options):
