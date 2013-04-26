97ce288c86ea3e8ad0de8c835d9322ceb4f1997e
Markus Lehtonen <markus.lehtonen@linux.intel.com>
2013-04-26 11:20:11 +0300

    buildpackage: implement --[no-]build cmdline option
    For enabling/disabling the builder script. The option is a bit
    asymmetric: --git-no-build automatically disables the 'postbuild' hook
    whereas --git-build does not enable it (e.g. if --git-no-hooks is
    given).
    
    Signed-off-by: Markus Lehtonen <markus.lehtonen@linux.intel.com>

--- gbp/config.py
+++ gbp/config.py
@@ -108,6 +108,7 @@ class GbpOptionParser(OptionParser):
                  'prebuild'        : '',
                  'postexport'      : '',
                  'postimport'      : '',
+                 'build'           : 'True',
                  'hooks'           : 'True',
                  'debian-tag'      : 'debian/%(version)s',
                  'debian-tag-msg'  : '%(pkg)s Debian release %(version)s',
@@ -275,6 +276,8 @@ class GbpOptionParser(OptionParser):
              'postimport':
                   ("hook run after a successful import, "
                    "default is '%(postimport)s'"),
+             'build':
+                  ("Enable running builder, default is %(build)s"),
              'hooks':
                   ("Enable running all hooks, default is %(hooks)s"),
              'time-machine':
