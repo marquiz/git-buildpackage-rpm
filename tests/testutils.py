f9c2cb4e0d2d373d53e703f1942667aa402404a2
Markus Lehtonen <markus.lehtonen@linux.intel.com>
2013-08-26 10:22:00 +0300

    import-orig: new function for filtering/re-packing sources
    Introduces a new function prepare_sources() that prepares upstream
    sources for importing into upstream branch and pristine-tar. That
    includes unpacking, filtering and re-packing sources. What somewhat
    complicates the logic is that it tries to avoid excess unpacking/packing
    of the sources.
    
    Also fixes the unpacking / filtering / repacking logic which was broken
    with some parameter combinations.
    
    Signed-off-by: Markus Lehtonen <markus.lehtonen@linux.intel.com>

--- tests/testutils.py
+++ tests/testutils.py
@@ -5,6 +5,7 @@ from . import context
 import os
 import shutil
 import subprocess
+import tarfile
 import tempfile
 import unittest
 
@@ -112,4 +113,25 @@ def get_dch_default_urgency():
         if os.path.isdir(tempdir):
             shutil.rmtree(tempdir)
     return urgency
+
+def ls_dir(directory):
+    """List the contents of directory, recurse to subdirectories"""
+    contents = set()
+    for root, dirs, files in os.walk(directory):
+        prefix = ''
+        if root != directory:
+            prefix = os.path.relpath(root, directory) + '/'
+        contents.update(['%s%s' % (prefix, fname) for fname in files] +
+                        ['%s%s' % (prefix, dname) for dname in dirs])
+    return contents
+
+def ls_tar(tarball):
+    """List the contents of tar archive"""
+    tmpdir = tempfile.mkdtemp()
+    try:
+        tarobj = tarfile.open(tarball, 'r')
+        tarobj.extractall(tmpdir)
+        return ls_dir(tmpdir)
+    finally:
+        shutil.rmtree(tmpdir)
 
