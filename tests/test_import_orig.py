4f48619080c8c22030fb083fcefc3672e713d585
Markus Lehtonen <markus.lehtonen@linux.intel.com>
2013-08-26 10:22:00 +0300

    import-orig: new function for filtering/re-packing sources
    Introduces a new function prepare_sources() that prepares upstream
    sources for importing into upstream branch and pristine-tar. That
    includes unpacking, filtering and re-packing sources. What somewhat
    complicates the logic is that it tries to avoid excess unpacking/packing
    of the sources.
    
    Also fixes the unpacking / filtering / repacking logic which was broken
    with some parameter combinations.
    
    Signed-off-by: Markus Lehtonen <markus.lehtonen@linux.intel.com>

--- tests/test_import_orig.py
+++ tests/test_import_orig.py
@@ -17,12 +17,17 @@
 """Test import-orig functions"""
 from . import context
 
+import glob
 import os
 import tarfile
+import tempfile
 import unittest
 
 from gbp.errors import GbpError
+from gbp.pkg import UpstreamSource
+from gbp.scripts.common.import_orig import prepare_sources
 from gbp.scripts.import_orig import find_source
+from tests.testutils import ls_dir, ls_tar
 
 
 class TestImportOrigBase(unittest.TestCase):
@@ -62,3 +67,124 @@ class TestFindSource(TestImportOrigBase):
         tarfile.open(tar_fn, 'w' ).close()
         self.assertEqual(os.path.abspath(tar_fn),
                          find_source(False, [tar_fn]).path)
+
+
+class TestPrepareSources(TestImportOrigBase):
+    """Test the prepare_sources() function"""
+    test_pkg_name = 'test'
+    test_pkg_ver = '1.0'
+
+    @staticmethod
+    def _create_test_sources(destdir):
+        """Create dummy source archives"""
+        destdir = os.path.abspath(destdir)
+        origs = {}
+
+        # "Normall" gzipped tarball
+        archive_fn = os.path.join(destdir, 'test-1.0.tar.gz')
+        src_dir = os.path.join(context.projectdir, 'gbp')
+        tarobj = tarfile.open(archive_fn, mode='w:gz')
+        for fname in (glob.glob('%s/*.py' % src_dir) +
+                      glob.glob('%s/pkg/*.py' % src_dir)):
+            arcname = 'test-1.0/' + os.path.relpath(fname, src_dir)
+            tarobj.add(fname, arcname=arcname)
+        tarobj.close()
+        origs['tar'] = archive_fn
+
+        # Unpacked sources
+        tarobj = tarfile.open(origs['tar'], 'r')
+        tarobj.extractall(destdir)
+        tarobj.close()
+        origs['dir'] = os.path.join(destdir,'test-1.0')
+        return origs
+
+    @classmethod
+    def setup_class(cls):
+        """Class set-up, run only once"""
+        super(TestPrepareSources, cls).setup_class()
+        # Different source archives
+        cls._origs = cls._create_test_sources(cls._tmpdir)
+
+    def test_dir(self):
+        """Basic test for unpacked sources, no filtering etc"""
+        tmpdir = tempfile.mkdtemp(dir=self._tmpdir, prefix='dir_basic_')
+        source = UpstreamSource(self._origs['dir'])
+        orig, prist = prepare_sources(source, 'test', '1.0', None,
+                                      None, False, None, tmpdir)
+        self.assertEqual(ls_dir(self._origs['dir']), ls_dir(orig))
+        self.assertEqual(prist, '')
+
+    def test_dir_filter(self):
+        """Test filtering of unpacked sources"""
+        tmpdir = tempfile.mkdtemp(dir=self._tmpdir, prefix='dir_filter_')
+        source = UpstreamSource(self._origs['dir'])
+        orig, prist = prepare_sources(source, 'test', '1.0', None,
+                                      ['pkg'], False, None, tmpdir)
+        orig_filt_ref = set([fname for fname in ls_dir(self._origs['dir'])
+                                    if not fname.startswith('pkg')])
+        self.assertEqual(orig_filt_ref, ls_dir(orig))
+        self.assertEqual(prist, '')
+
+    def test_dir_pristine_nofilter(self):
+        """Test filtering of unpacked sources, not filtering pristine-tar"""
+        tmpdir = tempfile.mkdtemp(dir=self._tmpdir, prefix='dir_filter2_')
+        source = UpstreamSource(self._origs['dir'])
+        orig, prist = prepare_sources(source, 'test', '1.0', 'test.tar.gz',
+                                      ['pkg'], False, None, tmpdir)
+        src_ls = ls_dir(self._origs['dir'])
+        orig_filt_ref = set([fname for fname in src_ls
+                                if not fname.startswith('pkg')])
+        prist_ref = set(['test-1.0/%s' % fname for fname in src_ls] +
+                        ['test-1.0'])
+        self.assertEqual(orig_filt_ref, ls_dir(orig))
+        self.assertEqual(prist_ref, ls_tar(prist))
+
+    def test_dir_pristine_filter(self):
+        """Test filtering pristine-tar and mangling prefix"""
+        tmpdir = tempfile.mkdtemp(dir=self._tmpdir, prefix='dir_filter3_')
+        source = UpstreamSource(self._origs['dir'])
+        orig, prist = prepare_sources(source, 'test', '1.0', 'test.tar.gz',
+                                      ['pkg'], True, 'newpref', tmpdir)
+        src_ls = ls_dir(self._origs['dir'])
+        orig_filt_ref = set([fname for fname in src_ls
+                                if not fname.startswith('pkg')])
+        prist_ref = set(['newpref/%s' % fname for fname in orig_filt_ref] +
+                        ['newpref'])
+        self.assertEqual(orig_filt_ref, ls_dir(orig))
+        self.assertEqual(prist_ref, ls_tar(prist))
+
+    def test_tar(self):
+        """Basic test for tarball sources, with pristine-tar"""
+        tmpdir = tempfile.mkdtemp(dir=self._tmpdir, prefix='tar_basic_')
+        source = UpstreamSource(self._origs['tar'])
+        orig, prist = prepare_sources(source, 'test', '1.0', 'test.tgz',
+                                      None, False, 'test-1.0', tmpdir)
+        src_ls = ls_tar(self._origs['tar'])
+        orig_ref = set([fname.replace('test-1.0/', '') for fname in src_ls
+                        if fname != 'test-1.0'])
+        self.assertEqual(orig_ref, ls_dir(orig))
+        self.assertEqual(src_ls, ls_tar(prist))
+
+    def test_tar_pristine_prefix(self):
+        """Test tarball import with prefix mangling"""
+        tmpdir = tempfile.mkdtemp(dir=self._tmpdir, prefix='tar_prefix_')
+        source = UpstreamSource(self._origs['tar'])
+        _orig, prist = prepare_sources(source, 'test', '1.0', 'test.tgz',
+                                       None, False, 'np', tmpdir)
+        src_ls = ls_tar(self._origs['tar'])
+        prist_ref = set([fname.replace('test-1.0', 'np') for fname in src_ls])
+        self.assertEqual(prist_ref, ls_tar(prist))
+
+    def test_tar_filter_pristine_prefix(self):
+        """Filter tarball, pristine-tar prefix mangling but not filter"""
+        tmpdir = tempfile.mkdtemp(dir=self._tmpdir, prefix='tar_filter_')
+        source = UpstreamSource(self._origs['tar'])
+        orig, prist = prepare_sources(source, 'test', '1.0', 'test.tgz',
+                                      ['pkg'], False, 'newp', tmpdir)
+        src_ls = ls_tar(self._origs['tar'])
+        orig_ref = set([fname.replace('test-1.0/', '') for fname in src_ls
+            if fname != 'test-1.0' and not fname.startswith('test-1.0/pkg')])
+        prist_ref = set([fname.replace('test-1.0', 'newp') for fname in src_ls])
+        self.assertEqual(orig_ref, ls_dir(orig))
+        self.assertEqual(prist_ref, ls_tar(prist))
+
