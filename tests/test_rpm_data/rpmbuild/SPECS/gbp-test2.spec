pombredanne/git-buildpackage-1

tests: add unittests for gbp.rpm
Signed-off-by: Markus Lehtonen <markus.lehtonen@linux.intel.com>

tests/test_rpm_data/rpmbuild/SPECS/gbp-test2.spec
@@ -1,0 +1,43 @@
+Name:       gbp-test2
+Summary:    Test package 2 for git-buildpackage
+Epoch:      2
+Version:    3.0
+Release:    0
+Group:      Development/Libraries
+License:    GPLv2
+Source10:   ftp://ftp.host.com/%{name}-%{version}.tar.gz
+Source:     foo.txt
+Source20:   bar.tar.gz
+# GbpIgnorePatch: 0
+Patch:      my.patch
+Patch10:    my2.patch
+Patch20:    my3.patch
+Packager:   Markus Letonen <markus.lehtone@linux.intel.com>
+
+%description
+Package for testing the RPM functionality of git-buildpackage.
+
+
+%prep
+%setup -T -n %{name}-%{version} -c -a 10
+
+%patch
+%patch -P 10 -p1
+
+
+%build
+make
+
+
+%install
+rm -rf %{buildroot}
+mkdir -p %{buildroot}/%{_datadir}/%{name}
+cp -R * %{buildroot}/%{_datadir}/%{name}
+install %{SOURCE0} %{buildroot}/%{_datadir}/%{name}
+
+
+
+%files
+%defattr(-,root,root,-)
+%dir %{_datadir}/%{name}
+%{_datadir}/%{name}

marquiz/git-buildpackage-rpm

tests/test_rpm_data/rpmbuild/SPECS/gbp-test2.spec
@@ -1,0 +1,43 @@
+Name:       gbp-test2
+Summary:    Test package 2 for git-buildpackage
+Epoch:      2
+Version:    3.0
+Release:    0
+Group:      Development/Libraries
+License:    GPLv2
+Source10:   ftp://ftp.host.com/%{name}-%{version}.tar.gz
+Source:     foo.txt
+Source20:   bar.tar.gz
+# GbpIgnorePatch: 0
+Patch:      my.patch
+Patch10:    my2.patch
+Patch20:    my3.patch
+Packager:   Markus Lehtonen <markus.lehtonen@linux.intel.com>
+
+%description
+Package for testing the RPM functionality of git-buildpackage.
+
+
+%prep
+%setup -T -n %{name}-%{version} -c -a 10
+
+%patch
+%patch -P 10 -p1
+
+
+%build
+make
+
+
+%install
+rm -rf %{buildroot}
+mkdir -p %{buildroot}/%{_datadir}/%{name}
+cp -R * %{buildroot}/%{_datadir}/%{name}
+install %{SOURCE0} %{buildroot}/%{_datadir}/%{name}
+
+
+
+%files
+%defattr(-,root,root,-)
+%dir %{_datadir}/%{name}
+%{_datadir}/%{name}