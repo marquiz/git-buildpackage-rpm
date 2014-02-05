de0b7909569ba3bbad7cf025cb196bcbc58c9188
Markus Lehtonen <markus.lehtonen@linux.intel.com>
2014-02-05 16:11:42 +0200

    GitRepository.commit_files: add committer_info argument
    For setting the committer name/email/date - similarly to author_info.
    
    Signed-off-by: Markus Lehtonen <markus.lehtonen@linux.intel.com>

--- gbp/git/repository.py
+++ gbp/git/repository.py
@@ -1355,8 +1355,11 @@ class GitRepository(object):
 
 #{ Comitting
 
-    def _commit(self, msg, args=[], author_info=None, edit=False):
+    def _commit(self, msg, args=[], author_info=None,
+                committer_info=None, edit=False):
         extra_env = author_info.get_author_env() if author_info else None
+        if committer_info:
+            extra_env.update(committer_info.get_committer_env())
         default_args = ['-q', '-m', msg] + (['--edit'] if edit else [])
         self._git_command("commit", default_args + args, extra_env=extra_env,
                           interactive=edit)
@@ -1384,7 +1387,7 @@ class GitRepository(object):
         """
         self._commit(msg=msg, args=['-a'], author_info=author_info, edit=edit)
 
-    def commit_files(self, files, msg, author_info=None):
+    def commit_files(self, files, msg, author_info=None, committer_info=None):
         """
         Commit the given files to the repository
 
@@ -1394,10 +1397,13 @@ class GitRepository(object):
         @type msg: C{str}
         @param author_info: authorship information
         @type author_info: L{GitModifier}
+        @param committer_info: committer information
+        @type committer_info: L{GitModifier}
         """
         if isinstance(files, six.string_types):
             files = [ files ]
-        self._commit(msg=msg, args=files, author_info=author_info)
+        self._commit(msg=msg, args=files, author_info=author_info,
+                     committer_info=committer_info)
 
     def commit_dir(self, unpack_dir, msg, branch, other_parents=None,
                    author={}, committer={}, create_missing_branch=False):
