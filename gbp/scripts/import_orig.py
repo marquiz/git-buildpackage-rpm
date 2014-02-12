e2c6991cb3613f9e765e1d15b0d3e6d9ee92e5e9
Markus Lehtonen <markus.lehtonen@linux.intel.com>
2014-02-12 15:11:30 +0200

    import-orig: implement --create-missing-branches option
    Create the upstream branch if it does not exist. Use the same option
    name that import-dsc has.
    
    Signed-off-by: Markus Lehtonen <markus.lehtonen@linux.intel.com>

--- gbp/scripts/import_orig.py
+++ gbp/scripts/import_orig.py
@@ -266,6 +266,9 @@ def build_parser(name):
                             help="Upstream VCS tag add to the merge commit")
     branch_group.add_boolean_config_file_option(option_name="merge", dest="merge")
     branch_group.add_config_file_option(option_name="merge-mode", dest="merge_mode")
+    branch_group.add_boolean_config_file_option(
+                      option_name="create-missing-branches",
+                      dest="create_missing_branches")
 
     tag_group.add_boolean_config_file_option(option_name="sign-tags",
                       dest="sign_tags")
@@ -361,7 +364,11 @@ def main(argv):
         is_empty = False if initial_branch else True
 
         if not repo.has_branch(options.upstream_branch) and not is_empty:
-            raise GbpError(no_upstream_branch_msg % options.upstream_branch)
+            if options.create_missing_branches:
+                gbp.log.info("Will create missing branch '%s'" %
+                             options.upstream_branch)
+            else:
+                raise GbpError(no_upstream_branch_msg % options.upstream_branch)
 
         (clean, out) = repo.is_clean()
         if not clean and not is_empty:
@@ -424,10 +431,10 @@ def main(argv):
                 parents = None
 
             commit = repo.commit_dir(source.unpacked,
-                                     msg=msg,
-                                     branch=import_branch,
-                                     other_parents=parents,
-                                     )
+                        msg=msg,
+                        branch=import_branch,
+                        other_parents=parents,
+                        create_missing_branch=options.create_missing_branches)
 
             if options.pristine_tar:
                 if pristine_orig:
