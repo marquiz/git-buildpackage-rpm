dce32709e80f1aa08feee687ad21afbd51756763
Markus Lehtonen <markus.lehtonen@linux.intel.com>
2014-04-10 11:30:06 +0300

    GitRepository: implement create_tree method
    New method for creating tree objects from a directory content.
    
    Change-Id: Idefbfcd2f188ea786269e7d50a10ada84c129234
    Signed-off-by: Markus Lehtonen <markus.lehtonen@linux.intel.com>

--- gbp/git/repository.py
+++ gbp/git/repository.py
@@ -1493,6 +1493,24 @@ class GitRepository(object):
         self._commit(msg=msg, args=args.args, author_info=author_info,
                      committer_info=committer_info, edit=edit)
 
+    def create_tree(self, unpack_dir):
+        """
+        Create a tree object out of a directory content
+
+        @param unpack_dir: content to add
+        @type unpack_dir: C{str}
+        @return: the tree object hash
+        @rtype: C{str}
+        """
+        git_index_file = os.path.join(self.path, self._git_dir, 'gbp_index')
+        try:
+            os.unlink(git_index_file)
+        except OSError:
+            pass
+        self.add_files('.', force=True, index_file=git_index_file,
+                       work_tree=unpack_dir)
+        return self.write_tree(git_index_file)
+
     def commit_dir(self, unpack_dir, msg, branch, other_parents=None,
                    author={}, committer={}, create_missing_branch=False):
         """
@@ -1515,15 +1533,7 @@ class GitRepository(object):
             doesn't already exist.
         @type create_missing_branch: C{bool}
         """
-
-        git_index_file = os.path.join(self.path, self._git_dir, 'gbp_index')
-        try:
-            os.unlink(git_index_file)
-        except OSError:
-            pass
-        self.add_files('.', force=True, index_file=git_index_file,
-                       work_tree=unpack_dir)
-        tree = self.write_tree(git_index_file)
+        tree = self.create_tree(unpack_dir)
 
         if branch:
             try:
