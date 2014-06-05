pombredanne/git-buildpackage-1

rpm: introduce new 'Gbp-Patch-Macros' special key in spec parsing
This can be used to mark location where to add new patch macros when
updating the spec.

Signed-off-by: Markus Lehtonen <markus.lehtonen@linux.intel.com>

gbp/rpm/__init__.py
@@ -271,6 +271,8 @@ class SpecFile(object):
                 if m.group('name').lower() == 'ignore-patches':
                     dataitems = m.group('args').strip().split()
                     ignorepatch = sorted([int(num) for num in dataitems])
+                elif m.group('name').lower() == 'patch-macros':
+                    ret['patchmacrostart'] = lineobj
                 else:
                     gbp.log.info("Found unrecognized Gbp tag on line %s: "
                                  "'%s'" % (i, line))
@@ -459,7 +461,10 @@ class SpecFile(object):
                                         "git-buildpackage:\n")
 
         # Determine where to add %patch macro lines
-        if last_removed_macro_line:
+        if 'patchmacrostart' in loc:
+            gbp.log.info("Adding patch macros after the start marker")
+            line = loc['patchmacrostart']
+        elif last_removed_macro_line:
             gbp.log.info("Adding patch macros in place of the removed macros")
             line = last_removed_macro_line
         elif 'lastpatchmacro' in loc:

marquiz/git-buildpackage-rpm

gbp/rpm/__init__.py
@@ -273,6 +273,8 @@ class SpecFile(object):
                 if m.group('name').lower() == 'ignore-patches':
                     dataitems = m.group('args').strip().split()
                     ignorepatch = sorted([int(num) for num in dataitems])
+                elif m.group('name').lower() == 'patch-macros':
+                    ret['patchmacrostart'] = lineobj
                 else:
                     gbp.log.info("Found unrecognized Gbp tag on line %s: "
                                  "'%s'" % (i, line))
@@ -461,7 +463,10 @@ class SpecFile(object):
                                         "git-buildpackage:\n")
 
         # Determine where to add %patch macro lines
-        if last_removed_macro_line:
+        if 'patchmacrostart' in loc:
+            gbp.log.debug("Adding patch macros after the start marker")
+            line = loc['patchmacrostart']
+        elif last_removed_macro_line:
             gbp.log.debug("Adding patch macros in place of the removed macros")
             line = last_removed_macro_line
         elif 'lastpatchmacro' in loc: