64352000dd27733e961b2dce6fb511636a5b5073
Markus Lehtonen <markus.lehtonen@linux.intel.com>
2014-06-26 13:35:19 +0300

    ComponentTestBase: ignore system and user config
    User might have a system and/or user specific gbp config files. Disable
    these config files so that they don't affect the component tests.
    
    Change-Id: I04b83680016690ffa5a2433ad16e9968e54f6f4a
    Signed-off-by: Markus Lehtonen <markus.lehtonen@linux.intel.com>

--- tests/component/__init__.py
+++ tests/component/__init__.py
@@ -88,6 +88,9 @@ class ComponentTestBase(object):
         # Create a top-level tmpdir for the test
         cls._tmproot = tempfile.mkdtemp(prefix='gbp_%s_' % cls.__name__,
                                         dir='.')
+        # Prevent local config files from messing up the tests
+        os.environ['GBP_CONF_FILES'] = '%(top_dir)s/.gbp.conf:' \
+                            '%(top_dir)s/debian/gbp.conf:%(git_dir)s/gbp.conf'
 
     @classmethod
     def teardown_class(cls):
