826ab04197db4eab6eba4115d40a62bf1c75f205
Markus Lehtonen <markus.lehtonen@linux.intel.com>
2014-06-26 10:05:01 +0300

    ComponentTestBase: add ls_tree() method
    Signed-off-by: Markus Lehtonen <markus.lehtonen@linux.intel.com>

--- tests/component/__init__.py
+++ tests/component/__init__.py
@@ -137,6 +137,13 @@ class ComponentTestBase(object):
                         (list(extra), list(missing))
         assert not extra and not missing, assert_msg
 
+    @staticmethod
+    def ls_tree(repo, treeish):
+        """List contents (blobs) in a git treeish"""
+        objs = repo.list_tree(treeish, True)
+        blobs = [obj[3] for obj in objs if obj[1] == 'blob']
+        return set(blobs)
+
     @classmethod
     def _check_repo_state(cls, repo, current_branch, branches, files=None):
         """Check that repository is clean and given branches exist"""
